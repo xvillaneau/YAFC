29cdb38ba9c0cbb7ecf421cd3712b98aa38a6581
Xavier Villaneau <xvillaneau@gmail.com>
2016-10-16 14:36:05 -0400

    Added YAML file with some (not all) item definitions

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+import setuptools
+
+setuptools.setup(
+    name="YAFC",
+    version="0.1",
+    description="Basic (an somewhat restricted) calculator for Factorio",
+    author="Xavier Villaneau",
+    author_email="xvillaneau@gmail.com",
+    license="MIT",
+    packages=["yafc"],
+    install_requires=['pyYAML']
+)
