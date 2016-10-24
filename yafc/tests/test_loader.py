5e6e4644beb7d6e61f094c70c2d04ed713b7ca89
Xavier Villaneau <xvillaneau@gmail.com>
2016-10-23 21:09:47 -0400

    Got 100% coverage on the loader module

--- yafc/tests/test_loader.py
+++ yafc/tests/test_loader.py
@@ -2,7 +2,8 @@
 import unittest
 from yafc.loader import GameSet
 
-from yafc.item import Mineral
+from yafc.item import Mineral, Manufactured, Item
+from yafc.machine import Assembler
 
 
 BASIC_YAML = """
@@ -49,3 +50,38 @@ class TestLoader(unittest.TestCase):
         self.assertIsInstance(mineral, Mineral)
         self.assertEqual(0.9, mineral.hardness)
         self.assertEqual(2.0, mineral.time)
+
+    def test_load_manufactured(self):
+        gs = GameSet("""
+        Mineral:
+          type: mining
+          hardness: 0.9
+          mining time: 2.0
+        Item:
+          ingredients:
+            Mineral: 2
+            Item 2: 1
+          time: 0.5
+          produced: 2
+        Item 2:
+          type: null
+        """)
+        item = gs.items.get("Item")
+        mineral = gs.items.get("Mineral")
+        self.assertIsInstance(item, Manufactured)
+        self.assertEqual(0.5, item.time)
+        self.assertEqual(2.0, item.produced)
+        self.assertIn(mineral, item.ingredients)
+
+    def test_load_machine(self):
+        gs = GameSet("""
+        Machine:
+          type: null
+          machine:
+            type: assembling
+            speed: 1.25
+        """)
+        machine = gs.machines.get("Machine")
+        self.assertIsInstance(machine, Assembler)
+        self.assertEqual("Machine", machine.item.name)
+        self.assertEqual(1.25, machine.speed)
